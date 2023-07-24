3fbf710727cdb18c770f57166d81c7b51adb4b30
gauravgeorgian <200520343@student.georgianc.on.ca>
2023-07-24 09:04:40 +0530

    files added

---
+++ MDEV1004-M2023-ICE8/MDEV1004-M2023-ICE8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MDEV1004-M2023-ICE8
+//
+//  Created by Tom Tsiliopoulos on 2023-07-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
